b13aa1c4e63bbeba11fca1275f43c2377a199a3b
Joel Morin <SKYELAIRD@HOTMAIL.COM>
2022-03-24 18:37:02 -0400

    est initial

---
+++ Ex_Files_Python_EssT/Exercise Files/Chap03/types.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = (1,2,3,4,5,6,7,8)
+print('x is {}'.format(x))
+print(type(x))
---
+++ brisque.py
@@ -0,0 +1,21 @@
+import collections
+from itertools import chain
+import urllib.request as request
+import pickle 
+
+import numpy as np
+
+import scipy.signal as signal
+import scipy.special as special
+import scipy.optimize as optimize
+
+import matplotlib.pyplot as plt
+
+import skimage.io
+import skimage.transform
+
+import cv2
+
+import libsvm
+
+print("yo")
