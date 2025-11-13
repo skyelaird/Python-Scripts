81b1c122437e995d22e85277683e33481f8a1e9e
skyelaird <37260519+skyelaird@users.noreply.github.com>
2025-11-13 13:30:51 -0400

    Variety of Python Scripts
    to be organized...

--- Ex_Files_Python_Excel/Exercise Files/fileRead.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from openpyxl.workbook import Workbook
-
-df = pd.read_csv("Names.csv",header=None)
-df.columns = ['First','Last','Address','City','State','area','number']
-
-print(df['First'][0:3])
